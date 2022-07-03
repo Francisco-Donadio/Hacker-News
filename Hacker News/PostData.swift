714b5fbdda087e3cd9589f527b20a5cc8236582a
Francisco Donadio <francisco.donadio@hotmail.com>
2022-07-03 14:59:48 -0300

    Initial Commit

---
+++ Hacker News/PostData.swift
@@ -0,0 +1,23 @@
+//
+//  PostData.swift
+//  Hacker News
+//
+//  Created by Francisco Donadio on 20/05/2022.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
